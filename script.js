cf2ba23e126960fe95867a7d4b0351e5bbaea70a
Samuel Mereau <53167217+SamuelMereau@users.noreply.github.com>
2020-11-26 18:15:34 +1100

    front-end design (half complete)

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-function add() {}
+function add(...num) {
+  //Adds (at least) two
+  if (num.length < 2) {
+    return 'ERROR, Less than two numbers';
+  }
+  return num.reduce((a, b) => a + b, 0);
+}
 
-function subtract() {}
+function subtract(...num) {
+  //Subtracts (at least) two numbers
+  if (num.length < 2) {
+    return 'ERROR, Less than two numbers';
+  }
+  return num.reduce((a, b) => a - b, 0);
+}
 
-function multiply() {}
+function multiply(...num) {
+  //Multiplies (at least) two numbers
+  if (num.length < 2) {
+    return 'ERROR, Less than two numbers';
+  }
+  return num.reduce((a, b) => a * b, 0);
+}
 
-function divide() {}
+function divide(...num) {
+  //Divides (at least) two numbers
+  if (num.length < 2) {
+    return 'ERROR, Less than two numbers';
+  }
+  return num.reduce((a, b) => a / b, 0);
+}
 
-function operate() {}
+function operate(...operation) {
+  //Takes an operator (+, -, *, /) and two numbers and then calls one of the above functions on the numbers
+}
