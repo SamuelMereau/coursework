4182cc3d6e85863213615693f1451a35ba02a2b4
SamuelMereau <mereau_s@students.gpcc.nsw.edu.au>
2021-07-10 20:32:43 +1000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
-//old code, pls copy new from dist
-
 const menuButton = document.querySelector('#menu-button');
-const projects = document.querySelector('#projects');
-menuButton.addEventListener('click', function () {
-    projects.classList.toggle('clicked');
-})
+const closeMenuButton = document.querySelector('#close-menu')
 
+function toggleClicked() {
+    const projects = document.querySelector('#projects');
+    projects.classList.toggle('clicked');
+}
+
+function toggleClickedOutside() {
+    toggleClicked();
+    const backdrop = document.querySelector('#backdrop');
+    backdrop.remove();
+}
+
+menuButton.addEventListener('click', function () {
+    toggleClicked()
+    const div = document.createElement('div');
+    div.id = 'backdrop';
+    div.onclick = toggleClickedOutside;
+    document.body.appendChild(div);
+})
+closeMenuButton.addEventListener('click', function () {
+    toggleClicked()
+    const backdrop = document.querySelector('#backdrop');
+    backdrop.remove();
+})
